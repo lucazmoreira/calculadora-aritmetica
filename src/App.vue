334581386c8af3295a621e8778d8f81cc43d73f8
Lucas Moreira <lucazmoreira@gmail.com>
2024-01-22 17:58:37 -0300

    commit inicial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,106 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="calculator">
+      <label for="num1">Número 1:</label>
+      <input
+        v-model="num1"
+        type="number"
+        id="num1"
+        @input="calcularResultado"
+      />
+
+      <label for="operacao">Operação:</label>
+      <select v-model="operacao" id="operacao" @change="calcularResultado">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+
+      <label for="num2">Número 2:</label>
+      <input
+        v-model="num2"
+        type="number"
+        id="num2"
+        @input="calcularResultado"
+      />
+
+      <p>Resultado: {{ resultado }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operacao: "+",
+      resultado: 0,
+    };
+  },
+  methods: {
+    calcularResultado() {
+      switch (this.operacao) {
+        case "+":
+          this.resultado = parseFloat(this.num1) + parseFloat(this.num2);
+          break;
+        case "-":
+          this.resultado = parseFloat(this.num1) - parseFloat(this.num2);
+          break;
+        case "*":
+          this.resultado = parseFloat(this.num1) * parseFloat(this.num2);
+          break;
+        case "/":
+          this.resultado = parseFloat(this.num1) / parseFloat(this.num2);
+          break;
+        default:
+          this.resultado = 0;
+      }
+    },
+  },
+};
 </script>
 
 <style>
+* {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #c0c0c0;
+}
+
+.calculator {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+
+label {
+  display: block;
+  margin-bottom: 8px;
+}
+
+input,
+select {
+  font-size: 16px;
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 16px;
+  box-sizing: border-box;
+}
+
+p {
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 20px;
 }
 </style>
